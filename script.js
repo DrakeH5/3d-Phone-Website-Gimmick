af5b05d2b7bb1f8dc379b9fea4079dda14943904
DrakeH5 <68834232+DrakeH5@users.noreply.github.com>
2022-10-22 19:15:53 -0500

    Initialized project

---
+++ script.js
@@ -0,0 +1,41 @@
+
+var scene = new THREE.Scene(); //created a three.js scene
+
+var camera = new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight,0.1,1000) //create a camera
+camera.position.z = 5;
+
+var renderer = new THREE.WebGLRenderer({antialias: true}); //create renderer
+renderer.setClearColor("darkgrey"); //essentially a background color
+renderer.setSize(window.innerWidth,window.innerHeight); //set renderer size to size of the page
+
+document.body.appendChild(renderer.domElement); //creates a canvas with our render's specifications
+
+window.addEventListener('resize', () => { //when the page is resized
+  renderer.setSize(window.innerWidth,window.innerHeight); //resize the renderer accordingly
+  camera.aspect = window.innerWidth / window.innerHeight; //re adjust aspect ratio
+
+  camera.updateProjectionMatrix(); //just updating our camera
+})
+
+
+
+var geometry = new THREE.BoxGeometry(1, 5, 7); //creates "phone"
+var material = new THREE.MeshLambertMaterial({color: "black"}); //creates material
+var phone = new THREE.Mesh(geometry, material); //creates a mesh out of our sphere and material
+phone.position.set(2, 0, -10)
+phone.rotation.y = -45
+
+scene.add(phone); //adds phone to the scene
+
+
+var light = new THREE.PointLight(0xFFFFFF, 1, 500); //creates a light (color, intensity, distance)
+light.position.set(10, 0, 25); //sets light position (x, y, z)
+scene.add(light); //add the light to the scene
+
+
+var render = function() {
+  requestAnimationFrame(render);
+  renderer.render(scene, camera); //just render (show) everything on our canvas
+}
+
+render();
